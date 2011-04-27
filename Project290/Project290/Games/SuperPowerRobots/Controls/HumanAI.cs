84825ea26e01042add223266e267f22985621b23
icd3 <git@iandimayuga.com>
2011-04-27 00:26:35 -0400

    SPRAI "Controls" class
    SPRAI is available for Bots to use for input. Also, one implementation of SPRAI, HumanAI, takes controller input

---
+++ Project290/Project290/Games/SuperPowerRobots/Controls/HumanAI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Project290.GameElements;
+using Project290.Inputs;
+
+namespace Project290.Games.SuperPowerRobots.Controls
+{
+    class HumanAI:SPRAI
+    {
+        public void Update(float dTime)
+        {
+            this.Move = new Vector2(GameWorld.controller.ContainsFloat(ActionType.MoveHorizontal), GameWorld.controller.ContainsFloat(ActionType.MoveVertical));
+            this.Fire = new Vector2(GameWorld.controller.ContainsFloat(ActionType.LookHorizontal), GameWorld.controller.ContainsFloat(ActionType.LookVertical));
+
+            this.Spin = GameWorld.controller.ContainsFloat(ActionType.LeftTrigger) - GameWorld.controller.ContainsFloat(ActionType.RightTrigger);
+
+            this.Weapons[0] = GameWorld.controller.ContainsBool(ActionType.BButton);
+            this.Weapons[1] = GameWorld.controller.ContainsBool(ActionType.YButton);
+            this.Weapons[2] = GameWorld.controller.ContainsBool(ActionType.XButton);
+            this.Weapons[3] = GameWorld.controller.ContainsBool(ActionType.AButton);
+        }
+    }
+}
--- Project290/Project290/Games/SuperPowerRobots/Controls/SPRAI.cs
+++ Project290/Project290/Games/SuperPowerRobots/Controls/SPRAI.cs
@@ -2,10 +2,77 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Project290.Games.SuperPowerRobots.Entities;
 
 namespace Project290.Games.SuperPowerRobots.Controls
 {
-    class SPRAI
+    //The base controls class for Bots.
+    abstract class SPRAI
     {
+        private Vector2 m_Move;
+        private Vector2 m_Fire;
+        private float m_Spin;
+        private bool[] m_Weapons;
+        protected SPRWorld m_World;
+
+        public SPRAI(SPRWorld world)
+        {
+            m_World = world;
+            m_Weapons = new bool[4];
+        }
+
+        /// <summary>
+        /// Movement vector, length between 0 and 1. "Left Joystick"
+        /// </summary>
+        public Vector2 Move {
+            get { return m_Move; }
+            protected set {
+                m_Move = value;
+                if (m_Move.LengthSquared() > 1)
+                    m_Move.Normalize();
+            }
+        }
+
+        /// <summary>
+        /// Firing vector, length between 0 and 1. "Right Joystick"
+        /// </summary>
+        public Vector2 Fire
+        {
+            get { return m_Fire; }
+            protected set
+            {
+                m_Fire = value;
+                if (m_Fire.LengthSquared() > 1)
+                    m_Fire.Normalize();
+            }
+        }
+
+        /// <summary>
+        /// Counterclockwise angular impulse (relative to max, between -1 and 1.) "Left Trigger minus Right Trigger"
+        /// </summary>
+        public float Spin
+        {
+            get { return m_Spin; }
+            protected set
+            {
+                m_Spin = Math.Max(Math.Min(value, 1), -1); //clamp between -1 and 1
+            }
+        }
+
+        /// <summary>
+        /// For each weapon, true if weapon is firing, false if not.
+        /// 0 = "B", 1 = "Y", 2 = "X", 3 = "A"
+        /// </summary>
+        public bool[] Weapons
+        {
+            get { return m_Weapons; }
+        }
+
+        /// <summary>
+        /// Updates the AI.
+        /// </summary>
+        /// <param name="dTime">Time elapsed.</param>
+        public abstract void Update(float dTime, Bot self);
     }
 }
